4014bf7b269cd31a7bb10672ba5c526130c56d9d
Mauro Peixoto <96122844+mauropeixotodev@users.noreply.github.com>
2024-05-11 10:34:39 -0300

    Archunit

---
+++ src/test/java/com/mauro/hexagonal/architecture/LayeredArchitectureTeste.java
@@ -0,0 +1,12 @@
+package com.mauro.hexagonal.architecture;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LayeredArchitectureTeste {
+	
+	
+	
+	
+
+}
